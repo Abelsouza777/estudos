0e5b091dc005dadebfb16cdeb2b6f384b62f20d3
Abelsouza777 <abel.sza@hotmail.com>
2025-01-16 21:41:41 -0300

    postendpoint

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express()
 app.use(express.json())
 
 //getsimples endpoint
-app.get("/hello",(req,res) => {
-    res.send("oi")
+app.get("/",(req,res) => {
+  res.send("meu servidor")
 })
 
+app.get("/hello",(req,res) => {
+   res.send("oi")
+})
+
+app.post("/hello",(req,res) => {
+  const nome = req.body.nome
+  res.send("oi" + nome)
+})
+
+// rodar o servidor
 app.listen(3000,() =>{
     console.log("servidor rodando na port 3000")
 })
