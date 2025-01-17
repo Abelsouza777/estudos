7f79e4d29f5bf7c6163de08063014a63a14df08c
Abelsouza777 <abel.sza@hotmail.com>
2025-01-16 21:24:20 -0300

    primeiro endpoint

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log ("oi")
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+//getsimples endpoint
+app.get("/hello",(req,res) => {
+    res.send("oi")
+})
+
+app.listen(3000,() =>{
+    console.log("servidor rodando na port 3000")
+})
